43f2ef105ac471554edaf203d51d1fe999760445
Julio Pazzaglia <julitopazzaglia@gmail.com>
2023-10-30 07:43:44 -0300

    commit message

---
+++ src/app/components/navbar.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <nav class='fade w-full rounded-bl-xl flex flex-row items-center'>
+      <Link href={"./"}>
+        {/* Scroll to top?? */}
+        <Image
+          src={"/assets/LogoLataBlanco.png"}
+          alt='logo'
+          width='64'
+          height='64'
+          className='p-4'
+        ></Image>
+      </Link>
+      <h1 className="p-4">Projects</h1>
+      <h1 className="p-4">About me</h1>
+      <h1 className="p-4">Contact me</h1>
+    </nav>
+  );
+}
